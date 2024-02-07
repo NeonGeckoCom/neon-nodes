b6e0fac2d8879a48b26fd75e389575ad00004b36
Daniel McKnight <daniel@neon.ai>
2024-02-07 13:28:09 -0800

    Fix typo in requirements

--- setup.py
+++ setup.py
@@ -73,8 +73,8 @@ setup(
     author_email='developers@neon.ai',
     license='BSD-3-Clause',
     packages=find_packages(),
-    requires=get_requirements("requirements.txt"),
-    extras_require={"voice_client": get_requirements("voice_client.txt")},
+    install_requires=get_requirements("requirements.txt"),
+    extras_require={"voice-client": get_requirements("voice_client.txt")},
     package_data={'neon_nodes': ['res/*']},
     entry_points={
         "console_scripts": ['neon-node=neon_nodes.cli:neon_node']
